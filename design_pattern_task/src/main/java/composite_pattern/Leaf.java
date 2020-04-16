ebeb560801e7c410709606d8bff06afcef368f09
mrinal365 <40198111+mrinal365@users.noreply.github.com>
2020-04-16 16:31:04 +0530

    Task done

---
+++ design_pattern_task/src/main/java/composite_pattern/Leaf.java
@@ -0,0 +1,25 @@
+package composite_pattern;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.maven_designpattern_task.design_pattern_task.Client;
+
+
+public class Leaf implements Component{
+
+	private static Logger Logger = LogManager.getLogger(Client.class);
+	int price;
+	String name;
+	public void showPrice() {
+		// TODO Auto-generated method stub
+		Logger.info(name + ":" + price);
+		//Logger.info(price);
+	}
+	public Leaf(int price, String name) {
+		super();
+		this.price = price;
+		this.name = name;
+	}
+
+}
